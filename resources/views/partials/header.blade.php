28e45d6a51d16d78d3f8f052361c87f9b50a95fe
BeneArp <benedetta.arpino@gmail.com>
2024-09-10 15:45:40 +0200

    feat: add header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,40 @@
+<header>
+
+    <div class="container">
+        <img src="/img/dc-logo.png" alt="Logo DC">
+
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Home</a>
+            </li>
+        </ul>
+    </div>
+
+</header>
